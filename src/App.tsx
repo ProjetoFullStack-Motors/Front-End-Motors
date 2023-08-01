a992238a092181a6ad692f102ec077d9a99a481c
Julia Otomo Duarte <julia_otomoduarte@outlook.com>
2023-08-01 14:33:41 -0300

    initial front-end structure with all libs added

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "react-toastify/dist/ReactToastify.css";
+import { GlobalStyles } from "./Styles/globalStyles";
+import AppRoutes from "./Routes";
+
+function App() {
+  return (
+    <>
+      <GlobalStyles />
+      <AppRoutes />
+    </>
+  );
+}
+
+export default App;
---
+++ src/Routes/index.tsx
@@ -0,0 +1,7 @@
+import { Routes, Route } from "react-router-dom";
+
+const AppRoutes = () => {
+  return <Routes></Routes>;
+};
+
+export default AppRoutes;
