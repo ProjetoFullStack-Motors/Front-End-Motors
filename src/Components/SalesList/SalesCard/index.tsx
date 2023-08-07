f160701f2c74158aa9264a3a3b520b3a11acbb35
Vitor Hugo Mendes <vitor_hugorj@hotmail.com>
2023-08-07 18:45:53 -0300

    feat: salesList and SalesCard components

--- src/Components/SalesList/SalesCard/index.tsx
+++ src/Components/SalesList/SalesCard/index.tsx
@@ -1,64 +1,71 @@
 import { StyledSalesCard } from "./style";
 
 type TSalesCard = {
-	sale: {
-		id: number;
-		brand: string;
-		model: string;
-		year: number;
-		mileage: number;
-		isGoodPrice: boolean;
-		price: number;
-		color: string;
-		description: string;
-		seller: {
-			firstName: string;
-			lastName: string;
-			img: string;
-		};
-		imgUrl: string;
-	};
+    sale: {
+        id: number;
+        brand: string;
+        model: string;
+        year: number;
+        mileage: number;
+        isGoodPrice: boolean;
+        price: number;
+        color: string;
+        description: string;
+        seller: {
+            firstName: string;
+            lastName: string;
+            img: string;
+        };
+        imgUrl: string;
+    };
 };
 
 const SalesCard = ({ sale }: TSalesCard) => {
-	const {
-		brand,
-		model,
-		year,
-		mileage,
-		isGoodPrice,
-		price,
-		description,
-		seller,
-		imgUrl,
-	} = sale;
-	return (
-		<StyledSalesCard>
-			<div className="car-img-container">
-				<img src={imgUrl} />
-				{isGoodPrice ? <img /> : null}
-			</div>
-			<h2>
-				{brand} - {model}
-			</h2>
-			<p>{description}</p>
-			<div className="user-info-container">
-				<div className="user-img-container">
-					<img src={seller.img}></img>
-				</div>
-				<h3>
-					{seller.firstName} {seller.lastName}
-				</h3>
-			</div>
-			<div>
-				<div>
-					<span>{mileage}</span>
-					<span>{year}</span>
-				</div>
-				<span>{price}</span>
-			</div>
-		</StyledSalesCard>
-	);
+    const {
+        brand,
+        model,
+        year,
+        mileage,
+        isGoodPrice,
+        price,
+        description,
+        seller,
+        imgUrl,
+    } = sale;
+    return (
+        <StyledSalesCard>
+            <div className="car-img-container">
+                <img className="car-img" src={imgUrl} />
+                {isGoodPrice ? (
+                    <h4
+                        className="good-price-tag"
+                        title="Essa oferta está 10% abaixo da tabela que esqueci o nome"
+                    >
+                        $
+                    </h4>
+                ) : null}
+            </div>
+            <h2 className="car-title">
+                {brand} - {model}
+            </h2>
+            <p className="car-description">{description}</p>
+            <div className="seller-info-container">
+                <div className="seller-img-container">
+                    <img className="seller-img" src={seller.img}></img>
+                </div>
+                <h3 className="seller-title">
+                    {seller.firstName} {seller.lastName}
+                </h3>
+            </div>
+            <div className="car-info-container">
+                <div className="car-info">
+                    <span>{mileage} KM</span>
+                    <span>{year}</span>
+                </div>
+                <span className="car-price">R$ {price},00</span>
+            </div>
+        </StyledSalesCard>
+    );
 };
 
 export default SalesCard;
