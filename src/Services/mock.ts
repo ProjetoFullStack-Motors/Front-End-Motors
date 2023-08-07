5548077bff0e1ee02dccf489ad1dd45968856ff8
Vitor Hugo Mendes <vitor_hugorj@hotmail.com>
2023-08-07 11:09:32 -0300

    creating cars mock

---
+++ src/Services/mock.ts
@@ -0,0 +1,56 @@
+const mock = [
+  {
+    id: 1,
+    brand: 'Fiat',
+    model: 'Uno',
+    year: 2019,
+    mileage: 0,
+    isGoodPrice: true,
+    price: 100000,
+    color: 'Azul',
+    description:
+      'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, compre 4 cartas imediatamente',
+    seller: {
+      firstName: 'Roberto',
+      lastName: 'Alberto',
+    },
+    imgUrl:
+      'https://storage.googleapis.com/ludopedia-imagens-jogo/1717a_8052.jpg',
+  },
+  {
+    id: 2,
+    brand: 'Celta',
+    model: 'Preto',
+    year: 2005,
+    mileage: 3,
+    isGoodPrice: false,
+    price: 200000,
+    color: 'Preto',
+    description:
+      'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, miou a praia legal, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets',
+    seller: {
+      firstName: 'Phill',
+      lastName: 'Connor',
+    },
+    imgUrl:
+      'https://ilhacarioca.com/wp-content/uploads/2021/12/Previs%C3%A3o-de-tempo-nublado-e-pancadas-de-chuva-at%C3%A9-sexta-dia-31.jpg',
+  },
+  {
+    id: 3,
+    brand: 'Brasília',
+    model: 'Amarela',
+    year: 1960,
+    mileage: 0,
+    isGoodPrice: false,
+    price: 200000,
+    color: 'Amarela',
+    description:
+      'Lorem Ipsum is simply dummy text of the printing and typesetting industry. insira aqui uma piada política Lorem Ipsum has been the industrys standard dummy text ever since the 1500s.',
+    seller: {
+      firstName: 'Povo',
+      lastName: 'Brasileiro',
+    },
+    imgUrl:
+      'https://s2-g1.glbimg.com/1gfWxBCiFD1x-dXXI-drC8Ryzls=/0x0:1280x857/1000x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_59edd422c0c84a879bd37670ae4f538a/internal_photos/bs/2021/v/R/VDITLGTFG7c20nAAwgog/whatsapp-image-2021-12-22-at-13.26.56.jpeg',
+  },
+];
