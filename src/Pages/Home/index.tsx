a5a99a24ce4962411e44182b67ab8d3ad147f6d5
Rogermferr <rogermferr@gmail.com>
2023-08-09 21:36:22 -0300

    feat: create banner home page

--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -9,6 +9,7 @@ import {
 import { ButtonContainerPosition, StyledHomePage } from "./style";
 import { createPortal } from "react-dom";
 import { useCarContext } from "../../Hooks";
+import Banner from "../../Components/Banner";
 
 const Home = () => {
   const { filterModal, setFilterModal } = useCarContext();
@@ -16,6 +17,7 @@ const Home = () => {
     <>
       <StyledHomePage>
         <Header />
+        <Banner />
         <div className="home-container">
           <AsideDesktop />
           <SalesList />
