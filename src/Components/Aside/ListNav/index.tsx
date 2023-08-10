9cbeb6c1bda4e16e66814634ca2f7cc990f9d01d
Julia Otomo Duarte <julia_otomoduarte@outlook.com>
2023-08-10 17:10:55 -0300

    trying to add filter pagination

--- src/Components/Aside/ListNav/index.tsx
+++ src/Components/Aside/ListNav/index.tsx
@@ -6,14 +6,39 @@ import { StyledNavList } from "./style";
 
 const ListNav = ({ saleKey, name }: TListNavProps) => {
   const { allCars } = useCarContext();
-  const uniqueItems = new Set(allCars.map((item) => item[saleKey]));
+  const unwantedKeys = [
+    "description",
+    "created_at",
+    "id",
+    "mileage",
+    "isGoodPrice",
+    "price",
+    "seller",
+    "salesImages",
+  ];
+
+  const uniqueItems = new Set(
+    allCars.map((item) => {
+      if (!unwantedKeys.includes(saleKey)) {
+        return item[saleKey];
+      }
+    })
+  );
+
   let clickedRef = useRef<any>(null);
 
   const handleClickRef = () => {
     clickedRef.current?.click();
   };
 
-  const uniqueItemsArray = Array.from(uniqueItems);
+  const uniqueItemsArray = Array.from(uniqueItems) as Array<string | number>;
+
+  if (uniqueItemsArray.every((item) => typeof item == "number")) {
+    uniqueItemsArray.sort((a, b) => Number(b) - Number(a));
+  } else {
+    uniqueItemsArray.sort();
+  }
+
   return (
     <StyledNavList>
       <h2>{name}</h2>
