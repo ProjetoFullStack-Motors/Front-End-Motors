3e9777f2f82ac2a8149a4bbd57ef4f1e37fec07d
Vitor Hugo Mendes <vitor_hugorj@hotmail.com>
2023-08-14 11:36:47 -0300

    feat: modal context and hook

---
+++ src/Providers/ModalContext/index.tsx
@@ -0,0 +1,25 @@
+import { createContext, useState } from "react";
+import {
+    TModalProviderProps,
+    TModalOptions,
+    TModalContextValues,
+} from "./@types";
+
+const ModalContext = createContext({} as TModalContextValues);
+
+const ModalProvider = ({ children }: TModalProviderProps) => {
+    const [modal, setModal] = useState<TModalOptions>(null);
+
+    return (
+        <ModalContext.Provider
+            value={{
+                modal,
+                setModal,
+            }}
+        >
+            {children}
+        </ModalContext.Provider>
+    );
+};
+
+export { ModalContext, ModalProvider };
