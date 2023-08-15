8d650e8bd992576b8d2de872b6f24e572baa0a62
Vitor Hugo Mendes <vitor_hugorj@hotmail.com>
2023-08-15 17:06:49 -0300

    feat: making the SalesList component dynamic

--- src/Components/SalesList/SalesCard/@types.ts
+++ src/Components/SalesList/SalesCard/@types.ts
@@ -2,6 +2,7 @@ import { TSaleProps } from "../../../Providers/CarContext/@types";
 
 type TSaleCardProps = {
     sale: TSaleProps;
+    owner: "all" | "seller";
 };
 
 export type { TSaleCardProps };
