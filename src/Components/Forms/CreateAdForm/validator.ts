afa0b5dfe459a7f0e0ab8ac7c57f7199d5823b82
Julia Otomo Duarte <julia_otomoduarte@outlook.com>
2023-08-16 11:05:03 -0300

    feat/create_ad: create ad form styled

--- src/Components/Forms/CreateAdForm/validator.ts
+++ src/Components/Forms/CreateAdForm/validator.ts
@@ -5,11 +5,6 @@ export const createAdSchema = z.object({
   color: z.string().nonempty("Campo obrigatório"),
   price: z.string().nonempty("Campo obrigatório"),
   description: z.string().nonempty("Campo obrigatório"),
-  brand: z.string().optional(),
-  model: z.string().optional(),
-  year: z.string().optional(),
-  fuel: z.string().optional(),
-  fipePrice: z.string().optional(),
 });
 
 export type TCreateAd = z.infer<typeof createAdSchema>;
