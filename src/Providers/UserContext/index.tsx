bb80b55cb6f6dca5a9d9a31ff3e5d60ccf0a6d0a
Cleyviton <cleyvitonkeke@gmail.com>
2023-08-22 17:07:11 -0300

    feat: implements profileViewUser page

--- src/Providers/UserContext/index.tsx
+++ src/Providers/UserContext/index.tsx
@@ -1,13 +1,13 @@
 import { createContext, useEffect, useState } from "react";
 import {
-  TUserContext,
-  TUserDataToken,
-  TUserName,
-  TUser,
-  TUserProvidersProps,
-  TErrorResponse,
-  TUserMail,
-  TJwtDecode,
+    TUserContext,
+    TUserDataToken,
+    TUserName,
+    TUser,
+    TUserProvidersProps,
+    TErrorResponse,
+    TUserMail,
+    TJwtDecode,
 } from "./@types";
 import { toast } from "react-toastify";
 import { useNavigate } from "react-router-dom";
@@ -21,106 +21,127 @@ import { TUserRegisterData } from "../../Components/Forms/RegisterForm/validator
 const UserContext = createContext({} as TUserContext);
 
 const UserProvider = ({ children }: TUserProvidersProps) => {
-  const navigate = useNavigate();
-  const [userName, setUserName] = useState<TUserName | null>(null);
-  const [user, setUser] = useState<TUser | null>(null);
+    const navigate = useNavigate();
+    const [userName, setUserName] = useState<TUserName | null>(null);
+    const [user, setUser] = useState<TUser | null>(null);
 
-  const { setModal } = useModal();
+    const { setModal } = useModal();
 
-  const userLogin = async (data: TLoginData) => {
-    try {
-      const response = await api.post("/users/login", data);
-      const { token } = response.data;
+    const userLogin = async (data: TLoginData) => {
+        try {
+            const response = await api.post("/users/login", data);
+            const { token } = response.data;
 
-      const tokenDecoded: TJwtDecode = jwt_decode(token);
+            const tokenDecoded: TJwtDecode = jwt_decode(token);
 
-      api.defaults.headers.common.Authorization = `Bearer ${token}`;
+            api.defaults.headers.common.Authorization = `Bearer ${token}`;
 
-      localStorage.setItem("frontEndMotors:token", token);
-      toast.success("Login realizado com sucesso!");
-      tokenDecoded.role === "seller" ? navigate("/dashboard") : navigate("/");
-    } catch (error) {
-      toast.error("E-mail ou senha inválido(s)!");
-      console.log(error);
-    }
-  };
+            localStorage.setItem("frontEndMotors:token", token);
+            toast.success("Login realizado com sucesso!");
 
-  const userRegister = async (data: TUserRegisterData) => {
-    try {
-      await api.post("/users", data);
-      const token = localStorage.getItem("frontEndMotors:token");
+            retrieveUser(tokenDecoded.userId);
 
-      api.defaults.headers.common.Authorization = `Bearer ${token}`;
+            tokenDecoded.role === "seller"
+                ? navigate("/dashboard")
+                : navigate("/");
+        } catch (error) {
+            toast.error("E-mail ou senha inválido(s)!");
+            console.log(error);
+        }
+    };
 
-      toast.success("Usuário cadastrado com sucesso!");
-      navigate("dashboard");
-    } catch (error) {
-      toast.error("Cadastro inválido!");
-      console.log(error);
-    }
-  };
+    const userRegister = async (data: TUserRegisterData) => {
+        try {
+            await api.post("/users", data);
+            const token = localStorage.getItem("frontEndMotors:token");
 
-  const logoutUser = () => {
-    localStorage.removeItem("frontEndMotors:token");
-    navigate("/");
-    setUser(null);
-  };
+            api.defaults.headers.common.Authorization = `Bearer ${token}`;
 
-  const retrieveUser = async (id: string) => {
-    try {
-      const response = await api.get(`users/${id}`);
-      setUserName({
-        firstName: response.data.firstName,
-        lastName: response.data.lastName,
-      });
-      setUser(response.data);
-    } catch (error) {
-      const currentError = error as AxiosError<TErrorResponse>;
-      toast.error(currentError.response?.data.message);
-      logoutUser();
-      console.log(error);
-    }
-  };
+            toast.success("Usuário cadastrado com sucesso!");
+            navigate("dashboard");
+        } catch (error) {
+            toast.error("Cadastro inválido!");
+            console.log(error);
+        }
+    };
 
-  const recoverPassword = async (data: TUserMail) => {
-    try {
-      await api.post("/recoverPass", data);
-      setModal(null);
-      navigate("/login");
-      toast.success("Senha enviado por e-mail");
-    } catch (error) {
-      const currentError = error as AxiosError<TErrorResponse>;
-      toast.error(currentError.response?.data.message);
-      console.log(error);
-    }
-  };
+    const logoutUser = () => {
+        localStorage.removeItem("frontEndMotors:token");
+        navigate("/");
+        setUser(null);
+    };
 
-  useEffect(() => {
-    const token = localStorage.getItem("frontEndMotors:token") || null;
-    const userData = token ? jwt_decode<TUserDataToken>(token) : null;
-    if (!userData) {
-      return;
-    }
-    api.defaults.headers.common.Authorization = `Bearer ${token}`;
-    retrieveUser(userData?.sub!);
-  }, []);
+    const retrieveUser = async (id: string) => {
+        try {
+            const response = await api.get(`users/${id}`);
+            setUserName({
+                firstName: response.data.firstName,
+                lastName: response.data.lastName,
+            });
+            setUser(response.data);
+        } catch (error) {
+            const currentError = error as AxiosError<TErrorResponse>;
+            toast.error(currentError.response?.data.message);
+            logoutUser();
+            console.log(error);
+        }
+    };
 
-  return (
-    <UserContext.Provider
-      value={{
-        userName,
-        user,
-        userLogin,
-        logoutUser,
-        recoverPassword,
-        userRegister,
-        setUser,
-        retrieveUser,
-      }}
-    >
-      {children}
-    </UserContext.Provider>
-  );
+    const retrieveProfileViewUser = async (
+        id: string,
+        setState: React.Dispatch<React.SetStateAction<TUser | null>>
+    ) => {
+        try {
+            const response = await api.get(`users/${id}`);
+            setState(response.data);
+        } catch (error) {
+            const currentError = error as AxiosError<TErrorResponse>;
+            toast.error(currentError.response?.data.message);
+
+            console.log(error);
+        }
+    };
+
+    const recoverPassword = async (data: TUserMail) => {
+        try {
+            await api.post("/recoverPass", data);
+            setModal(null);
+            navigate("/login");
+            toast.success("Senha enviado por e-mail");
+        } catch (error) {
+            const currentError = error as AxiosError<TErrorResponse>;
+            toast.error(currentError.response?.data.message);
+            console.log(error);
+        }
+    };
+
+    useEffect(() => {
+        const token = localStorage.getItem("frontEndMotors:token") || null;
+        const userData = token ? jwt_decode<TUserDataToken>(token) : null;
+        if (!userData) {
+            return;
+        }
+        api.defaults.headers.common.Authorization = `Bearer ${token}`;
+        retrieveUser(userData?.sub!);
+    }, []);
+
+    return (
+        <UserContext.Provider
+            value={{
+                userName,
+                user,
+                userLogin,
+                logoutUser,
+                recoverPassword,
+                userRegister,
+                setUser,
+                retrieveUser,
+                retrieveProfileViewUser,
+            }}
+        >
+            {children}
+        </UserContext.Provider>
+    );
 };
 
 export { UserContext, UserProvider };
--- src/Routes/index.tsx
+++ src/Routes/index.tsx
@@ -2,19 +2,21 @@ import { Routes, Route } from "react-router-dom";
 import { Dashboard, Home, Register, Login } from "../Pages";
 import ProtectedRoute from "../Pages/ProtectedRoute";
 import Sale from "../Pages/Sale";
+import ProfileViewUser from "../Pages/ProfileViewUser";
 
 const AppRoutes = () => {
-  return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/register" element={<Register />} />
-      <Route path="/sale/:id" element={<Sale />} />
-      <Route path="/dashboard" element={<ProtectedRoute />}>
-        <Route index element={<Dashboard />} />
-      </Route>
-    </Routes>
-  );
+    return (
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/sale/:id" element={<Sale />} />
+            <Route path="/dashboard" element={<ProtectedRoute />}>
+                <Route index element={<Dashboard />} />
+            </Route>
+            <Route path="/ProfileViewUser/:id" element={<ProfileViewUser />} />
+        </Routes>
+    );
 };
 
 export default AppRoutes;
