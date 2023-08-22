39efd494f6ebbcc6e94699ff620f0527ec6a7a61
Vitor Hugo Mendes <vitor_hugorj@hotmail.com>
2023-08-22 14:15:32 -0300

    feat: salesComments user post section

--- src/Components/SaleComments/style.ts
+++ src/Components/SaleComments/style.ts
@@ -3,6 +3,8 @@ import styled from "styled-components";
 export const StyledSaleComments = styled.div`
     display: flex;
     flex-direction: column;
+    padding: 30px;
+    gap: 40px;
 
     .comments-section {
         display: flex;
@@ -15,9 +17,67 @@ export const StyledSaleComments = styled.div`
         line-height: 170%;
         font-weight: var(--font-semibold);
     }
+
     .comments-list {
         display: flex;
         flex-direction: column;
         gap: 25px;
+    }
+
+    .comments-post {
+        display: flex;
+        flex-direction: column;
+        gap: 10px;
+    }
+
+    .user-header {
+        display: flex;
+        gap: 10px;
+        align-items: center;
+    }
+
+    .username {
+        font-size: var(--font-body-1);
+        line-height: 170%;
+        font-weight: var(--font-medium);
+        color: var(--grey-1);
+    }
+
+    .message-container {
+        position: relative;
+        width: 100%;
+    }
+
+    .message-area {
+        padding: 20px;
+        font-family: "Lexend", "sans-serif";
+        font-size: var(--font-body-1);
+        font-weight: var(--font-base);
+        color: var(--grey-3);
+        width: 100%;
+        min-height: 200px;
+        border: 1.5px solid var(--grey-7);
+        outline-color: var(--brand-1);
+        resize: none;
+    }
+
+    .post-button {
+        position: absolute;
+        right: 15px;
+        bottom: 15px;
+        border-radius: 4px;
+    }
+
+    .message-suggestions {
+        display: flex;
+        gap: 10px;
+    }
+
+    .message-suggestions > span {
+        padding: 8px;
+        color: var(--grey-3);
+        background-color: var(--grey-7);
+        border-radius: 12px;
+        cursor: pointer;
     }
 `;
