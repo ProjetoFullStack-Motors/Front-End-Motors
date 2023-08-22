ddc68a99655c449bf4abb4c556bc5b898453dc17
Rogermferr <rogermferr@gmail.com>
2023-08-21 22:22:14 -0300

    feat: add modal from sale images

--- src/Components/SaleContainer/ListImages/index.tsx
+++ src/Components/SaleContainer/ListImages/index.tsx
@@ -1,23 +1,48 @@
+import { createPortal } from "react-dom";
+import { useModal } from "../../../Hooks";
 import { TSaleContainerProps } from "../@types";
+import ModalImage from "./ModalImage";
 import { StyleListImages } from "./style";
+import { useState } from "react";
+import { Modal } from "../../";
 
 const ListImages = ({ saleFounded }: TSaleContainerProps) => {
+  const { modal, setModal } = useModal();
+
+  const [url, setUrl] = useState("");
+
   const findImage = (url: string) => {
     const image = saleFounded.salesImages.find((img) => img.imageUrl === url);
-    return image;
+    setUrl(image!.imageUrl);
+    setModal("Imagem do veículo");
   };
 
   return (
-    <StyleListImages>
-      <h2>Fotos</h2>
-      <ul>
-        {saleFounded.salesImages.map((img) => (
-          <li key={img.imageUrl} onClick={() => findImage(img.imageUrl)}>
-            <img src={img.imageUrl} alt={saleFounded.model} />
-          </li>
-        ))}
-      </ul>
-    </StyleListImages>
+    <>
+      <StyleListImages>
+        <h2>Fotos</h2>
+        <ul>
+          {saleFounded.salesImages.map((img) => (
+            <li
+              key={img.imageUrl}
+              title="Visualizar imagem"
+              onClick={() => findImage(img.imageUrl)}>
+              <img src={img.imageUrl} alt={saleFounded.model} />
+            </li>
+          ))}
+        </ul>
+      </StyleListImages>
+
+      {modal && modal == "Imagem do veículo"
+        ? createPortal(
+            <Modal title="Imagem do veículo">
+              {" "}
+              <ModalImage url={url} />
+            </Modal>,
+            document.body
+          )
+        : null}
+    </>
   );
 };
 
