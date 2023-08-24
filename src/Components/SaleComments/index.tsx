ae80800c0247dccc00fc55f3256c5be97d4d805b
Rogermferr <rogermferr@gmail.com>
2023-08-24 19:24:43 -0300

    fix: add keys from comments

--- src/Components/SaleComments/index.tsx
+++ src/Components/SaleComments/index.tsx
@@ -5,45 +5,45 @@ import SaleCommentCard from "./SaleCommentCard";
 import { Button, UserAvatar } from "../index";
 
 const SaleComments = () => {
-    const { user } = useUserContext();
+  const { user } = useUserContext();
 
-    const postSuggestions = [
-        "Gostei muito!",
-        "Incrível",
-        "Recomendarei para meus amigos!",
-    ];
+  const postSuggestions = [
+    "Gostei muito!",
+    "Incrível",
+    "Recomendarei para meus amigos!",
+  ];
 
-    return (
-        <StyledSaleComments>
-            <div className="comments-section">
-                <h2>Comentários</h2>
-                <ul className="comments-list">
-                    {comments.map((comment) => (
-                        <SaleCommentCard comment={comment} />
-                    ))}
-                </ul>
-            </div>
-            <div className="comments-post">
-                <div className="user-header">
-                    <UserAvatar
-                        username={`${user?.firstName} ${user?.lastName}`}
-                        img={user?.userImage}
-                    />
-                    <h3 className="username">{`${user?.firstName} ${user?.lastName}`}</h3>
-                </div>
+  return (
+    <StyledSaleComments>
+      <div className="comments-section">
+        <h2>Comentários</h2>
+        <ul className="comments-list">
+          {comments.map((comment) => (
+            <SaleCommentCard comment={comment} key={comment.created_at} />
+          ))}
+        </ul>
+      </div>
+      <div className="comments-post">
+        <div className="user-header">
+          <UserAvatar
+            username={`${user?.firstName} ${user?.lastName}`}
+            img={user?.userImage}
+          />
+          <h3 className="username">{`${user?.firstName} ${user?.lastName}`}</h3>
+        </div>
 
-                <div className="message-container">
-                    <textarea className="message-area"></textarea>
-                    <Button className="post-button">Comentar</Button>
-                </div>
-                <div className="message-suggestions">
-                    {postSuggestions.map((suggestion) => (
-                        <span>{suggestion}</span>
-                    ))}
-                </div>
-            </div>
-        </StyledSaleComments>
-    );
+        <div className="message-container">
+          <textarea className="message-area"></textarea>
+          <Button className="post-button">Comentar</Button>
+        </div>
+        <div className="message-suggestions">
+          {postSuggestions.map((suggestion, index) => (
+            <span key={index}>{suggestion}</span>
+          ))}
+        </div>
+      </div>
+    </StyledSaleComments>
+  );
 };
 
 export default SaleComments;
