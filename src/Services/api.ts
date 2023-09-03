b5f9710e701d6266d4d04fb1213151455fe3c00c
Fernandomtos <rmavali@bol.com.br>
2023-09-03 16:52:17 -0300

    feat: update salesAd

--- src/Services/api.ts
+++ src/Services/api.ts
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  // baseURL: "https://motorsm6.onrender.com",
-  baseURL: "http://localhost:3000",
+  baseURL: "https://motorsm6.onrender.com",
   timeout: 6000,
 });
 
